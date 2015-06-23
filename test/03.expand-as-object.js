bc2db53a2171be19b83f1bda3538df5920e2a516
Alexej Yaroshevich <alex@qfox.ru>
2015-06-24 02:15:37 +0300

    Misc: fixup repeating values assigning according to browsers

--- test/03.expand-as-object.js
+++ test/03.expand-as-object.js
@@ -1,6 +1,6 @@
 describe('expand as object', function () {
 
-    it('should expand border shorthand with a value and flag to an object', function () {
+    it('should expand border shorthand with a flag', function () {
         SC.expand('border', '1px solid red', false)
             .should.eql({
                 'border-width': '1px',
@@ -9,7 +9,7 @@ describe('expand as object', function () {
             });
     });
 
-    it('should expand border shorthand with a value to an object', function () {
+    it('should expand border shorthand', function () {
         SC.expand('border', '1px solid red')
             .should.eql({
                 'border-top-width': '1px',
@@ -24,6 +24,16 @@ describe('expand as object', function () {
                 'border-right-color': 'red',
                 'border-bottom-color': 'red',
                 'border-left-color': 'red'
+            });
+    });
+
+    it('should expand border-radius shorthand', function () {
+        SC.expand('border-radius', '0px 4em 3px')
+            .should.eql({
+                'border-top-left-radius': '0px',
+                'border-top-right-radius': '4em',
+                'border-bottom-right-radius': '3px',
+                'border-bottom-left-radius': '4em',
             });
     });
 
