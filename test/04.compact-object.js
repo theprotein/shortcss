e2990155dcd5748ef39fba25e8752f1c7de93bc4
Alexej Yaroshevich <alex@qfox.ru>
2015-06-24 17:31:58 +0300

    Misc: initial tests for compact

---
+++ test/04.compact-object.js
@@ -0,0 +1,76 @@
+describe('compact object', function () {
+
+    describe('for border-radius properties', function () {
+
+        it('should compact them with 1 value', function () {
+            SC.compact({
+                'border-top-left-radius': '9px',
+                'border-top-right-radius': '9px',
+                'border-bottom-right-radius': '9px',
+                'border-bottom-left-radius': '9px',
+            }).should.eql({
+                'border-radius': '9px',
+            });
+        });
+
+        it('should compact them partially', function () {
+            SC.compact({
+                'border-top-left-radius': '9px',
+                'border-bottom-left-radius': '9px',
+            }).should.eql({
+                'border-radius': '9px none none 9px',
+            });
+        });
+
+        it('should compact them with 2 different values', function () {
+            SC.compact({
+                'border-top-left-radius': '9px',
+                'border-top-right-radius': '12px',
+                'border-bottom-right-radius': '9px',
+                'border-bottom-left-radius': '12px',
+            }).should.eql({
+                'border-radius': '9px 12px',
+            });
+        });
+
+        it('should compact them with 3 different values', function () {
+            SC.compact({
+                'border-top-right-radius': '5px',
+                'border-bottom-right-radius': '10px',
+                'border-bottom-left-radius': '5px',
+            }).should.eql({
+                'border-radius': 'none 5px 10px',
+            });
+        });
+
+    });
+
+    describe('simplification of the same value', function () {
+
+        it('should simplify border-width value', function () {
+            SC.compact({
+                'border-width': '2px 2px 2px',
+            }).should.eql({
+                'border-width': '2px',
+            });
+        });
+
+        it('should simplify more complex border-width to 2 values', function () {
+            SC.compact({
+                'border-width': '0px 5px 0px 5px',
+            }).should.eql({
+                'border-width': '0px 5px',
+            });
+        });
+
+        it('should strip the last border-radius value to 3 values', function () {
+            SC.compact({
+                'border-radius': '0px 5px 10px 5px',
+            }).should.eql({
+                'border-radius': '0px 5px 10px',
+            });
+        });
+
+    });
+
+});
