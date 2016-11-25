870f5e435605b0534a91365f7a5cb401c08b5eb6
Yuri Tkachenko <yuri@ya.ru>
2016-11-25 18:41:32 +0200

    should compact an Array of props

--- test/04.compact-object.js
+++ test/04.compact-object.js
@@ -1,5 +1,14 @@
 describe('compact object', function () {
 
+    it('should compact an Array of props', function () {
+        SC.compact([
+            'border-top-left-radius',
+            'border-top-right-radius',
+            'border-bottom-right-radius',
+            'border-bottom-left-radius'
+        ]).should.eql('border-radius');
+    });
+
     describe('for border-radius properties', function () {
 
         it('should compact them with 1 value', function () {
